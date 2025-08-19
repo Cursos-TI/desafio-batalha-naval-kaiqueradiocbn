d5c3131e33ec3788b6e59362d7dcdc2af173dfd0
kaiqueradiocbn <kaique.radiocbn@gmail.com>
2025-08-19 21:01:44 +0000

    Tabuleiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -10,57 +10,28 @@ int main() {
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
-    char linha [10] = {'A', 'B', 'C', 'D', 'E', 'F','G', 'H', 'I', 'J'}; //ARRAY PARA IDENTIFICAR AS COLUNAS
+    char linha[10] = {'A','B','C','D','E','F','G','H','I','J'}; // ARRAY PARA IDENTIFICAR AS COLUNAS
     int tabuleiro[10][10] = {0}; // Matriz Tabuleiro
 
-    //Exibindo Tabuleiro
+    printf("Tabuleiro de batalha naval:\n");
 
-    printf("Tabuleiro de batalha Naval.\n"); // ANUNCIO DO JOGO
-    for ( int l = 0; l < 10; l++){ // LOOP FOR PARA IMPRESSÃO DAS LETRAS PARA LOCALIZAÇÃO
+    // Imprime as letras da linha de cima (colunas)
+
+printf("  "); // Espaço inicial para alinhar com os números da lateral
+    for (int l = 0; l < 10; l++) {  
         printf("%c ", linha[l]);
+    }
+    printf("\n"); // Pular linha se não os numeros começaram na mesma linha que as vogais
+
+    // Loop para imprimir o tabuleiro com números na lateral
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", i); // número da linha
+        for (int j = 0; j < 10; j++) {
+            printf("%d ", tabuleiro[i][j]);
+            
         }
-
-        printf("\n"); // PULAR LINHA DEPOIS DA LOCALIZAÇÃO
-
-
-    for ( int i = 0; i < 10; i++){   // LOOP FOR PARA A IMPRESSÃO DO TABULEIRO 
-    for ( int j = 0; j < 10; j++) {
-    printf ("%d ", tabuleiro[i][j]);
+        printf("\n");
     }
-    printf("\n");
-    }
-
-
-
-
-  
-
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
 
     return 0;
 }
